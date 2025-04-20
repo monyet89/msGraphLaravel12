4f655cc6a924d38dff321233c7f2d61f72a61337
monyet89 <made_in_madiun@yahoo.com>
2025-04-20 09:30:13 +0700

    Sample ENV added

--- routes/web.php
+++ routes/web.php
@@ -11,14 +11,6 @@ Route::get('/', [HomeController::class, 'welcome']);
 
 Route::get('/signin', [AuthController::class, 'signin']);
 Route::get('/callback', [AuthController::class, 'callback']);
-#Route::get('/signin', function () {
- #   return Socialite::driver('microsoft')->redirect();
-#});
-
-#Route::get('/signin/callback', function () {
-#    $user = Socialite::driver('microsoft')->user();
-#    return redirect('/'); // Avoid redirect loops here
-#});
 
 Route::get('/signout', [AuthController::class, 'signout']);
 Route::get('/calendar', [CalendarController::class, 'calendar']);
